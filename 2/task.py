28d459b9db96c3b6826e855baf4c3399e90f2b0b
alxsod <alexander.soderstrand@gmail.com>
2020-06-25 14:55:22 +0200

    day3 start

--- 2/task.py
+++ 2/task.py
@@ -25,28 +25,27 @@ first(opcode_arr)
 print("Answer 1: " + str(opcode_arr[0]))
 
 def second():
+    def inner(i,j):
+        opcodes_cs = open('input.txt','r')                                                            
+        opcode_arr = opcodes_cs.read().strip('\n').split(',')                                         
+        opcode_arr = [int(opcode) for opcode in opcode_arr]                                           
+        opcode_arr[1] = j                                                                             
+        opcode_arr[2] = i                                                                             
+        first(opcode_arr)                                                                             
+        return opcode_arr                                                                             
+                                                                                                      
     for i in range(0,99):
         for j in range(0,99):
-            opcodes_cs = open('input.txt','r')                   
-            opcode_arr = opcodes_cs.read().strip('\n').split(',')
-            opcode_arr = [int(opcode) for opcode in opcode_arr]                                     
-            opcode_arr[2] = j
-            opcode_arr[1] = i
-            first(opcode_arr)
-            if opcode_arr[0] == 19690720:
-                return_str = "Answer 2: " + str(100 * opcode_arr[1] + opcode_arr[2] )
+            return_arr= inner(i,j)
+            if return_arr[0] == 19690720:
+                return_str = "Answer 2: " + str(100 * return_arr[1] + return_arr[2] )
                 return return_str
        
     for i in range(0,99):                                                                                
          for j in range(0,99):                                                                           
-             opcodes_cs = open('input.txt','r')                                                        
-             opcode_arr = opcodes_cs.read().strip('\n').split(',')
-             opcode_arr = [int(opcode) for opcode in opcode_arr]
-             opcode_arr[1] = j
-             opcode_arr[2] = i
-             first(opcode_arr)
-             if opcode_arr[0] == 19690720:
-                return_str = "Answer 2: " + str(100 * opcode_arr[1] + opcode_arr[2])
+             return_arr = inner(j,i)
+             if return_arr[0] == 19690720:
+                return_str = "Answer 2: " + str(100 * return_arr[1] + return_arr[2])
                 return return_str 
 
 print(second())
